66ccab700c447b2fd6147dbd22733b91e1185e53
Anne Gambrel <agambrel@kicp.uchicago.edu>
2021-03-18 16:37:01 -0600

    adding full path to where it said xfaster is installed

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../xfaster"))
+sys.path.insert(0, "/usr/local/lib/python3.8/dist-packages")
 
 import xfaster
 # -- Project information -----------------------------------------------------
