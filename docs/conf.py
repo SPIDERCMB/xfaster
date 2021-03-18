ec87f5929dee2b4da100f57a3af14b683e688e49
Sasha Rahlin <arahlin@fnal.gov>
2021-03-18 15:54:49 +1300

    minor docs cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, '/home/anne/.pyenv/versions/3.8.2/envs/spider/lib/python3.8/site-packages')
 
 
 # -- Project information -----------------------------------------------------
