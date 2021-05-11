538d682d9caa1979d6065e0afdeb470d10355045
Anne E. Gambrel <agambrel@princeton.edu>
2021-05-11 23:04:10 +0200

    Put some config options back in run_xfaster to handle types right

--- scripts/gcorr/run_xfaster.py
+++ scripts/gcorr/run_xfaster.py
@@ -6,6 +6,21 @@ import xfaster as xf
 import argparse as ap
 from configparser import ConfigParser
 
+# Change XFaster options here to suit your purposes
+opts = dict(
+    likelihood=False,
+    residual_fit=False,
+    foreground_fit=False,
+    # change options below for your purposes
+    tbeb=True,
+    bin_width=25,
+    lmin=2,
+    lmax=500,
+)
+
+# Change submit options here to fit your system
+submit_opts = dict(nodes=1, ppn=1, mem=6, omp_threads=10, wallt=4)
+
 P = ap.ArgumentParser()
 P.add_argument("--gcorr-config", help="The config file for gcorr computation")
 P.add_argument("-f", "--first", default=0, type=int, help="First sim index to run")
@@ -27,53 +42,54 @@ P.add_argument("--check-point", default="bandpowers", help="XFaster checkpoint")
 P.add_argument(
     "--no-submit", dest="submit", action="store_false", help="Don't submit, run locally"
 )
-P.add_argument("--omp", default=None, type=int, help="Number of omp threads, if submit. Overwrites value in config file")
-
-output_root = "../../example/gcorr_run/"  # Set your own output root.
+P.add_argument(
+    "--omp",
+    default=None,
+    type=int,
+    help="Number of omp threads, if submit. Overwrites value in config file",
+)
 
 args = P.parse_args()
 
+# start by loading up gcorr config file and parsing it
 assert os.path.exists(args.gcorr_config), "Missing config file {}".format(
     args.gcorr_config
 )
 g_cfg = ConfigParser()
 g_cfg.read(args.gcorr_config)
+
+# set all user-specific xfaster opts
+for k, v in g_cfg["xfaster_opts"].items():
+    opts[k] = v
 null = g_cfg.getboolean("gcorr_opts", "null")
+tags = g_cfg["gcorr_opts"]["map_tags"].split(",")
+
+# null tests should use noise sims. signal shouldn't.
+if null:
+    opts["noise_type"] = g_cfg["xfaster_opts"]["noise_type"]
+else:
+    opts["noise_type"] = None
+
+opts["output_root"] = os.path.join(g_cfg["gcorr_opts"]["output_root"], args.output)
+
+# update opts with command line args
+opts["apply_gcorr"] = args.gcorr
+opts["reload_gcorr"] = args.reload_gcorr
+opts["checkpoint"] = args.check_point
 
 seeds = list(range(args.first, args.first + args.num))
 
-tags = g_cfg["gcorr_opts"]["map_tags"]
-
 for tag in tags:
-    opts = dict(
-        output_root=os.path.join(g_cfg["gcorr_opts"]["output_root"], args.output),
-        output_tag=tag,
-        likelihood=False,
-        residual_fit=False,
-        foreground_fit=False,
-        apply_gcorr=args.gcorr,
-        reload_gcorr=args.reload_gcorr,
-        checkpoint=args.check_point,
-    )
-    # set all user-specific xfaster opts
-    for k, v in g_cfg["xfaster_opts"].items():
-        opts[k] = v
+    opts["output_tag"] = tag
 
-    # null tests should use noise sims. signal shouldn't.
-    if null:
-        opts["noise_type"] = g_cfg["xfaster_opts"]["noise_type"]
-    else:
-        opts["noise_type"] = None
-
-    submit_opts = g_cfg["submit_opts"]
     if args.omp is not None:
-        submit_opts.update("omp_threads", args.omp)
+        submit_opts["omp_threads"] = args.omp
 
     if args.submit:
         opts.update(**submit_opts)
 
     for s in seeds:
-        opts.update(sim_index=s)
+        opts["sim_index"] = s
         if args.submit:
             xf.xfaster_submit(**opts)
         else:
