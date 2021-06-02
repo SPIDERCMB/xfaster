a84b77043ce39dd17b37032eb32cac15d7640e30
Anne Gambrel <anne.gambrel@gmail.com>
2021-06-02 09:16:30 -0600

    Completing docstrings for xfaster_tools.py

--- xfaster/xfaster_tools.py
+++ xfaster/xfaster_tools.py
@@ -13,37 +13,57 @@ __all__ = [
 
 
 def blackbody(nu, ref_freq=353.0):
+    """
+    The ratio of the blackbody function for dust at frequency nu
+    over the value for reference frequency ref_freq
+
+    Arguments
+    ---------
+    nu : float
+        Frequency in GHz.
+    ref_freq : float
+        Reference frequency in GHz.
+
+    Returns
+    -------
+    blackbody_ratio : float
+        B(nu, T_dust) / B(nu_ref, T_dust)
+    """
     k = 1.38064852e-23  # Boltzmann constant
     h = 6.626070040e-34  # Planck constant
     T = 19.6
     nu_ref = ref_freq * 1.0e9
-    # T = 2.725 #Cmb BB temp in K
-    nu *= 1.0e9  # Ghz
+    nu *= 1.0e9  # GHz -> Hz
     x = h * nu / k / T
     x_ref = h * nu_ref / k / T
     return x ** 3 / x_ref ** 3 * (np.exp(x_ref) - 1) / (np.exp(x) - 1)
 
 
-def rj2cmb(nu_in, ccorr=True):
+def rj2cmb(nu_in):
     """
-    planck_cc_gnu = {101: 1.30575, 141: 1.6835, 220: 3.2257, 359: 14.1835}
-    if ccorr:
-        if np.isscalar(nu_in):
-            for f, g in planck_cc_gnu.items():
-                if int(nu_in) == int(f):
-                    return g
+    Conversion from Rayleigh-Jeans units to CMB temperature units
+
+    Arguments
+    ---------
+    nu_in : float
+        Frequency in GHz.
+
+    Returns
+    -------
+    cal_fac : float
+        Number by which to multiply a RJ temperature to get a CMB temp
     """
     k = 1.38064852e-23  # Boltzmann constant
     h = 6.626070040e-34  # Planck constant
     T = 2.72548  # Cmb BB temp in K
-    nu = nu_in * 1.0e9  # Ghz
+    nu = nu_in * 1.0e9  # GHz -> Hz
     x = h * nu / k / T
     return (np.exp(x) - 1.0) ** 2 / (x ** 2 * np.exp(x))
 
 
 def scale_dust(freq0, freq1, ref_freq, beta, delta_beta=None, deriv=False):
     """
-    Get the factor by which you must dividide the cross spectrum from maps of
+    Get the factor by which you must multiply the cross spectrum from maps of
     frequencies freq0 and freq1 to match the dust power at ref_freq given
     spectra index beta.
 
@@ -52,6 +72,32 @@ def scale_dust(freq0, freq1, ref_freq, beta, delta_beta=None, deriv=False):
 
     Otherwise if delta_beta is given, return the scale factor adjusted
     for a linearized offset delta_beta from the reference beta.
+
+    Arguments
+    ---------
+    freq0 : float
+        Frequency of map0 in GHz.
+    freq1 : float
+        Frequency of map1 in GHz.
+    ref_freq : float
+        Reference frequency from which to compute relative scaling in GHz.
+    beta : float
+        Dust spectral index.
+    delta_beta : float
+        Difference from beta-- scaling computed as a first order Taylor
+        expansion from original beta-scaling.
+    deriv : bool
+        If true, return the frequency scaling at the reference beta, along with
+        the first derivative w.r.t. beta at the reference beta.
+
+    Returns
+    -------
+    freq_scale : float
+        The relative scaling factor for the dust cross spectrum-- multiply by
+        this number to get the dust spectrum at the reference frequency
+    -- or --
+    freq_scale, deriv : floats
+        The relative scaling factor and its derivative
     """
     freq_scale = (
         rj2cmb(freq0)
@@ -76,7 +122,7 @@ def wigner3j(l2, m2, l3, m3):
     Wigner 3j symbols computed for all valid values of ``L``, as in:
 
     .. math::
-    
+
         \begin{pmatrix}
          \ell_2 & \ell_3 & L \\
          m_2 & m_3 & 0 \\
